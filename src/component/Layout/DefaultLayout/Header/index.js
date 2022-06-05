a31d049757a576fb86e929a9d42516ea1ad1a3b8
hwanginreallive <hwangineallive@gmail.com>
2022-06-05 17:48:49 +0700

    commit

--- src/component/Layout/DefaultLayout/Header/index.js
+++ src/component/Layout/DefaultLayout/Header/index.js
@@ -4,7 +4,14 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* {logo} */}
+                {/* {search} */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
